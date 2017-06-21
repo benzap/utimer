490d38768bc7b155560021b7cf4f79e58b043371
Benjamin Zaporzan <benzaporzan@gmail.com>
2017-06-21 12:59:30 -0400

    lein new chestnut utimer +garden +rum

---
+++ dev/cljs/user.cljs
@@ -0,0 +1,8 @@
+(ns cljs.user
+  (:require [utimer.core]
+            [utimer.system :as system]))
+
+(def go system/go)
+(def reset system/reset)
+(def stop system/stop)
+(def start system/start)
---
+++ src/cljc/utimer/common.cljc
@@ -0,0 +1,7 @@
+(ns utimer.common)
+
+(defn shared-fn
+  "A function that is shared between clj and cljs"
+  []
+  (println "cljc!"))
+
